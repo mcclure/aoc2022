f08955891b3568453604b0947ac1244c8119485d
mcc <andi.m.mcclure@gmail.com>
2022-12-01 21:06:09 -0500

    calories line input

--- 1-1-calories/src/main.rs
+++ 1-1-calories/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, Error, ErrorKind, BufRead, BufReader};
+
+fn main() -> Result<(), Error> {
+	let filename = std::env::args().fuse().nth(1);
+	let input: BufReader = match &filename {
+		None => BufReader::new(stdin()),
+		Some(x) => BufReader::new(std::fs::File::open(x)?)
+	};
+
+	for line in input.lines() {
+		let line = line?;
+		if line.is_empty() {
+			println!("EMPTY")
+		} else {
+			let calories = line.parse::<i64>();
+			match calories {
+				Ok(_) => println!("INT"),
+				_ => return Err(Error::new(ErrorKind::InvalidInput, "Non-numeric input"))
+			}
+		}
+	}
+
+	Ok(())
 }
