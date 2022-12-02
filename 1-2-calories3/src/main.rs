c3113d977698e180bf408e6e2fe0ac9eac776c4f
mcc <andi.m.mcclure@gmail.com>
2022-12-01 22:25:41 -0500

    1-2-calories3 completed

--- 1-2-calories3/src/main.rs
+++ 1-2-calories3/src/main.rs
@@ -1,8 +1,11 @@
-// Take list containing chunks of numbers separated by newlines. Return sum of top chunk.
+// Take list containing chunks of numbers separated by newlines. Return sum of sums of top 3 chunks.
 
 use std::io::{BufRead, BufReader, Error, ErrorKind, Stdin, stdin};
 use std::fs::File;
 use either::Either;
+use std::collections::BinaryHeap;
+
+const SUM_OF:usize = 3;
 
 fn main() -> Result<(), Error> {
 	let filename = std::env::args().fuse().nth(1);
@@ -13,15 +16,13 @@ fn main() -> Result<(), Error> {
 
 	let mut lines = input.lines();
 
-	let mut current = 0;
-	let mut best = 0;
+	let mut current: i64 = 0;
+	let mut best = BinaryHeap::<i64>::with_capacity(SUM_OF);
 
 	loop {
 		let mut chunk_finished = || {
-			// println!("Chunk finished; {} > {}", current, best);
-			if current>best {
-				best = current
-			}
+			// println!("Chunk finished; {} > {}", current, best.peek());
+			best.push(current);
 			current = 0;
 		};
 
@@ -40,7 +41,24 @@ fn main() -> Result<(), Error> {
 		}
 	}
 
-	println!("{}", best);
+	{
+		let mut count = 0;
+		let mut total:i64 = 0;
+		loop {
+			total += match best.pop() {
+				None => break,
+				Some(x) => {
+					//println!("Summing {}", x);
+					x
+				} 
+			};
+			// Unfortunately, if you go over the capacity of BinaryHeap it just keeps growing!
+			// So this solution works, but is much less efficient than it could be.
+			count = count + 1;
+			if count >= SUM_OF { break }
+		}
+		println!("{}", total);
+	}
 
 	Ok(())
 }
