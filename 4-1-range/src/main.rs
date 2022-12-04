ef8c0577731bfb630709404619c34cb594738675
mcc <andi.m.mcclure@gmail.com>
2022-12-04 14:00:46 -0500

    pom implementation, does not work

--- 4-1-range/src/main.rs
+++ 4-1-range/src/main.rs
@@ -3,6 +3,11 @@ use std::io::{BufRead, BufReader, Error, ErrorKind, Stdin, stdin};
 use std::fs::File;
 use either::Either;
 
+use pom::parser::*;
+use pom::Parser;
+use std::str;
+use std::str::FromStr;
+
 fn main() -> Result<(), Error> {
     // Load file from command-line argument or (if none) stdin
 	let filename = std::env::args().fuse().nth(1);
@@ -15,11 +20,21 @@ fn main() -> Result<(), Error> {
 
 	let mut total: i64 = 0;
 
+	// p for positive
+	fn pinteger() -> Parser<u8, i64> {
+		let integer = one_of("123456789") - one_of("0123456789").repeat(0..) | sym('0');
+		integer.collect().convert(str::from_utf8).convert(|s|i64::from_str(&s))
+	}
+	let range = pinteger() - sym(b'-') + pinteger();
+	let range_pair = range - sym(b',') + range;
+
 //	let invalid = || { return Err(Error::new(ErrorKind::InvalidInput, "Expecting other")) };
 
 	// Scan file
 	for line in lines {
 		let line = line?;
+		let content = range_pair.parse(br#"Test"#);
+		println!("{:?}", content);
 	}
 
 	// Final score
