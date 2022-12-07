bb17d8df524baecd154b2d3807769a1a60e29532
mcc <andi.m.mcclure@gmail.com>
2022-12-07 10:15:31 -0500

    More filesize scaffold...

--- 7-1-filesize/src/main.rs
+++ 7-1-filesize/src/main.rs
@@ -2,26 +2,28 @@
 
 use std::io::{BufRead, BufReader, Error, ErrorKind, Stdin, stdin};
 use std::fs::File;
+use std::collections::HashMap;
 use either::Either;
+
+const BADSIZE:u64 = 100_000;
 
 #[derive(Default)]
 struct Dir {
-	dir:HashMap<Dir>,
-	size:i64
+	dir:HashMap<String, Dir>,
+	size:u64
 }
 
-fn print_tree(d:Dir, depth:usize) {
-	for 0..depth { print!("\t") }
-	println!()
-	for d2 in d.dir {
-		total += total_filesize(d2)
+fn print_tree(d:&Dir, depth:usize) {
+	for (k,v) in &d.dir {
+		for _ in 0..depth { print!("\t") }
+		println!("{}: {}", k, d.size);
+		print_tree(d, depth+1);
 	}
-	return total
 }
 
-fn total_filesize(d:Dir) {
+fn total_filesize(d:&Dir) -> u64 {
 	let mut total = d.size;
-	for d2 in d.dir {
+	for d2 in d.dir.values() {
 		total += total_filesize(d2)
 	}
 	return total
@@ -39,14 +41,26 @@ fn main() -> Result<(), Error> {
 
 	let mut root:Dir = Default::default();
 
-	// Scan file
-	for line in lines {
-		let line = line?;
+	{
+		let mut pwd:Vec<&mut Dir> = Vec::new();
+		pwd.push(&mut root);
+
+		//let invalid = || { return Err(Error::new(ErrorKind::InvalidInput, "Expecting other")) };
+
+		// Scan file
+		for line in lines {
+			let line = line?;
+		}
 	}
 
-	let mut total: i64 = 0;
+	print_tree(&root, 0);
 
-//	let invalid = || { return Err(Error::new(ErrorKind::InvalidInput, "Expecting other")) };
+	let mut total: u64 = 0;
+
+	for v in root.dir.values() {
+		let size = total_filesize(v);
+		if size > BADSIZE { total += size }
+	}
 
 	// Final score
 	println!("{}", total);
