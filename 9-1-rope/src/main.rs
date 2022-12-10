8ff02bff492cee7c1bbb16b1a5d3cb32196363c8
mcc <andi.m.mcclure@gmail.com>
2022-12-09 20:10:28 -0500

    up to ndarray

--- 9-1-rope/src/main.rs
+++ 9-1-rope/src/main.rs
@@ -3,9 +3,10 @@
 use std::io::{BufRead, BufReader, Error, ErrorKind, Stdin, stdin};
 use std::fs::File;
 use either::Either;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
+use hashbrown::HashMap;
+use hashbrown::hash_map::Entry;
 use itertools::Itertools;
+use std::cmp::{min, max};
 
 const DEBUG:bool = false;
 
@@ -13,11 +14,16 @@ const DEBUG:bool = false;
 macro_rules! d { ( $( $x:expr ),* ) => { if (DEBUG) { println!($($x,)*) } }; }
 macro_rules! ds { ( $( $x:expr ),* ) => { if (DEBUG) { print!($($x,)*) } }; }
 
-#[derive(PartialEq,PartialOrd)]
+#[derive(PartialEq,PartialOrd,Copy,Clone)]
 enum Cell { Empty, Headed, Tailed, Start }
 impl Default for Cell { fn default() -> Self { Cell::Empty } }
 
 type At = (i32,i32);
+
+fn point_add((x,y):At, (x2,y2):At) -> At { (x+x2,y+y2) }
+fn point_sub((x,y):At, (x2,y2):At) -> At { (x-x2,y-y2) }
+fn point_min((x,y):At, (x2,y2):At) -> At { (min(x,x2),min(y,y2)) }
+fn point_max((x,y):At, (x2,y2):At) -> At { (max(x,x2),max(y,y2)) }
 
 fn main() -> Result<(), Error> {
     // Load file from command-line argument or (if none) stdin
@@ -36,27 +42,20 @@ fn main() -> Result<(), Error> {
 	let invalide2 = || { Error::new(ErrorKind::InvalidInput, "Non-integer argument") };
 
 	let mut map: HashMap<At, Cell> = Default::default();
-	let map_write = |at:At,v:Cell| {
+	let mut map_write = |at:At,v:Cell| {
 		let entry = map.entry(at);
-		if match entry { Entry::Vacant(_) => true, Entry::Occupied(v2) => v>*v2.get()} {
-			entry.insert_entry(v);
+		if match &entry { Entry::Vacant(_) => true, Entry::Occupied(v2) => v>*v2.get()} {
+			entry.insert(v);
 		}
 	};
-
-	fn map_write_if_greater<K,T>(map:HashMap<K,T>, key:K, v:T) where K:Eq+std::hash::Hash, T:PartialEq+PartialOrd {
-		let entry = map.entry(key);
-		if match entry { Entry::Vacant(_) => true, Entry::Occupied(v2) => v>*v2.get()} {
-			entry.insert_entry(v);
-		}
-	}
 
 	// Scan file
 	{
 		let mut head_at = (0,0);
-		map_write_if_greater(map, head_at, Cell::Start);
+		map_write(head_at, Cell::Start);
 		for line in lines {
 			let line = line?;
-			let (dir_str, num_str) = line.split_whitespace().collect_tuple().unwrap_or_else(invalide)?;
+			let (dir_str, num_str) = line.split_whitespace().collect_tuple().ok_or_else(invalide)?;
 			let dir = 
 				match dir_str {
 					"U" => (0,-1), "D" => (0,1), "L" => (-1,0), "R" => (1,0),
@@ -64,9 +63,31 @@ fn main() -> Result<(), Error> {
 				};
 			let count = num_str.parse::<usize>().map_err(|_|invalide2())?;
 			for _ in 0..count {
-				head_at += dir;
+				head_at = point_add(head_at, dir);
 				map_write(head_at, Cell::Headed);
 			}
+		}
+	}
+
+	if DEBUG {
+		let (mut min, mut max) = ((i32::MIN, i32::MIN), (i32::MAX, i32::MAX));
+		for k in map.keys() {
+			min = point_min(min, *k);
+			max = point_max(min, *k);
+		}
+		let (xs, ys) = point_sub(max,min);
+		let mut grid = [[Cell::Empty; ys]; xs];
+		for (k,v) in map {
+			let (x,y) = point_min(k, min);
+			grid[y][x] = v;
+		}
+		for col in grid {
+			for v in col {
+				ds!("{}", match v {
+					Empty => '.', Headed => '_', Cell => '#', Start => 'S'
+				})
+			}
+			d!("");
 		}
 	}
 
