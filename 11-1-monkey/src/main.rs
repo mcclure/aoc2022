b27645b33d14178f7f633d04902a1748d376662b
mcc <andi.m.mcclure@gmail.com>
2022-12-11 10:27:32 -0500

    Read in file, do nothing else

--- 11-1-monkey/src/main.rs
+++ 11-1-monkey/src/main.rs
@@ -1,4 +1,4 @@
-// Summary
+// Parses a series of monkey descriptions.
 
 use std::io::{BufRead, BufReader, Error, ErrorKind, Stdin, stdin};
 use std::fs::File;
@@ -12,15 +12,25 @@ fn main() -> Result<(), Error> {
 		Some(x) => either::Right(BufReader::new(std::fs::File::open(x)?))
 	};
 
-	let lines = input.lines();
+	let mut lines = input.lines().filter(|x|match x { Ok(x) => !x.is_empty(), _ => true }).peekable();
 
 	let mut total: i64 = 0;
 
-//	let invalid = || { return Err(Error::new(ErrorKind::InvalidInput, "Expecting other")) };
+	{
+		use pom::parser::*;
 
-	// Scan file
-	for line in lines {
-		let line = line?;
+		fn next<I, T:Iterator<Item = Result<I, Error>>>(l:&mut T) -> Result<I, Error> { match (*l).next() { Some(x) => x, None => Err(Error::new(ErrorKind::InvalidInput, "Incomplete monkey")) } }
+
+		fn positive<'a>() -> Parser<'a, char, u64> {
+			let integer = one_of("123456789") - one_of("0123456789").repeat(0..) | sym('0');
+			integer.collect().convert(|s|String::from_iter(s.iter()).parse::<u64>())
+		}
+
+		// Scan file
+		loop {
+			let line = next(&mut lines)?;
+			// Do stuff to "line"
+		}
 	}
 
 	// Final score
