84142cf0c17328f093f2ae31c0fcdae7c804e892
mcc <andi.m.mcclure@gmail.com>
2022-12-18 18:35:06 -0500

    17-1-tetris but it doesn't work

--- 17-1-tetris/src/main.rs
+++ 17-1-tetris/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader, Error, ErrorKind, Stdin, stdin};
 use std::fs::File;
 use std::io::Read;
 use std::collections::HashSet;
+use std::cmp;
 use either::Either;
 use glam::IVec2;
 
@@ -26,7 +27,7 @@ const MINO_SPEC: &str = "\
 ##
 ##";
 
-const SIMULATION:i32 = 2022;
+const SIMULATION:usize = 2022;
 const WIDTH:i32 = 7;
 const SPAWN_X:i32 = 2; // 2 from left wall
 const SPAWN_Y:i32 = 3; // 3 above highest point
@@ -51,32 +52,76 @@ fn main() -> Result<(), Error> {
 			ctrl.push(match byte? {
 				b'>' => true,
 				b'<' => false,
-				c => return invalid(c)
+				c => if (c as char).is_whitespace() { 
+					break
+				} else {
+					return invalid(c)
+				}
 			});
 		}
+		ctrl
 	};
 
-	let minos = {
+	let (minos, mino_heights) = {
 		let mut minos: Vec<HashSet<IVec2>> = Default::default();
+		let mut mino_heights: Vec<i32> = Default::default();
 		let mut mino: HashSet<IVec2> = Default::default();
+		let mut height = 0;
 		for (y,line) in MINO_SPEC.lines().enumerate() {
 			if line.is_empty() {
 				minos.push(std::mem::take(&mut mino));
+				mino_heights.push(std::mem::take(&mut height));
 			} else {
 				for (x,ch) in line.chars().enumerate() {
 					mino.insert(IVec2::new(x as i32,y as i32));
 				}
+				height += 1;
 			}
 		}
 		minos.push(mino);
-		minos
+		mino_heights.push(height);
+		(minos, mino_heights)
 	};
 
 	let mut board: HashSet<IVec2> = Default::default();
 	let mut max:i32 = 0;
+	let mut at: Option<IVec2> = None; 
+	let mut mino_at = 0;
 
 	for t in 0..SIMULATION {
-
+		let mino = &minos[mino_at];
+		if at.is_none() { // New piece
+			at = Some(IVec2::new(SPAWN_X, max + SPAWN_Y + mino_heights[mino_at]));
+		}
+		match at {
+			Some(at_unwrap) => {
+				let right = ctrl[t % ctrl.len()];
+				let mut try_move = |v:IVec2, down:bool| {
+					let at_moved = at_unwrap + v;
+					for &cell in mino {
+						let at_cell = at_moved + cell;
+						if at_cell.x < 0 || at_cell.x >= WIDTH { return }
+						if board.contains(&at_cell) {
+							if down {
+								mino_at += 1;
+								mino_at %= minos.len();
+								for &cell in mino { // Shadow
+									let at_cell = at_unwrap + cell;
+									board.insert(at_cell);
+								}
+								max = cmp::max(max, at_unwrap.y);
+								at = None;
+							}
+							return
+						}
+					}
+					at = Some(at_moved);
+				};
+				try_move(if right { IVec2::X } else { -IVec2::X }, false);
+				try_move(-IVec2::Y, true);
+			}
+			_ => unreachable!()
+		}
 	}
 
 	// Final score
