5235ea8f8ebf976603cb1f6a7cb6e63ef071eb7d
mcc <andi.m.mcclure@gmail.com>
2022-12-18 12:19:44 -0500

    18-1-lava works

--- 18-1-lava/src/main.rs
+++ 18-1-lava/src/main.rs
@@ -2,7 +2,9 @@
 
 use std::io::{BufRead, BufReader, Error, ErrorKind, Stdin, stdin};
 use std::fs::File;
+use std::collections::HashSet;
 use either::Either;
+use glam::IVec3;
 
 fn main() -> Result<(), Error> {
     // Load file from command-line argument or (if -) stdin
@@ -17,11 +19,40 @@ fn main() -> Result<(), Error> {
 
 	let mut total: i64 = 0;
 
-//	let invalid = || { return Err(Error::new(ErrorKind::InvalidInput, "Expecting other")) };
+	let invalid = |s| { return Error::new(ErrorKind::InvalidInput, format!("Line must be list of 3 comma-separated numbers, but got '{}'", s)) };
+
+	let mut vox:HashSet<IVec3> = Default::default();
+	let (mut min, mut max) = (IVec3::new(i32::MAX, i32::MAX, i32::MAX), IVec3::new(i32::MIN, i32::MIN, i32::MIN));
 
 	// Scan file
 	for line in lines {
 		let line = line?;
+		let i3 = line.split(",").map(|x|x.parse::<i32>().map_err(|_|invalid(line.clone()))).collect::<Result<Vec<i32>,Error>>()?;
+		if i3.len() != 3 { return Err(invalid(line)) }
+		let v = IVec3::from_slice(&i3[0..3]);
+		vox.insert(v);
+		min = min.min(v);
+		max = max.max(v);
+	}
+
+	let cardinals = [IVec3::new(-1,0,0), IVec3::new(1,0,0),
+				     IVec3::new(0,-1,0), IVec3::new(0,1,0),
+				     IVec3::new(0,0,-1), IVec3::new(0,0,1)];
+
+	for z in min.z..=max.z {
+		for y in min.y..=max.y {
+			for x in min.x..=max.x {
+				let at = IVec3::new(x,y,z);
+				if vox.contains(&at) {
+					for card in cardinals {
+						let at = at + card;
+						if !vox.contains(&at) {
+							total += 1;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	// Final score
