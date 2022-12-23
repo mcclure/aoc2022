e970ffab7050fd71b2d53000354dc5dab7cca87d
mcc <andi.m.mcclure@gmail.com>
2022-12-23 00:48:30 -0500

    Diffuse printout

--- 23-1-diffuse/src/main.rs
+++ 23-1-diffuse/src/main.rs
@@ -2,30 +2,76 @@
 
 use std::io::{BufRead, BufReader, Error, ErrorKind, Stdin, stdin};
 use std::fs::File;
+use std::collections::HashSet;
 use either::Either;
+use glam::IVec2;
+
+// Set 0 to disable
+const DEBUG_ROUND:usize = 10;
 
 fn main() -> Result<(), Error> {
     // Load file from command-line argument or (if -) stdin
-	let filename = std::env::args().fuse().nth(1);
-	let input: Either<BufReader<Stdin>, BufReader<File>> = match filename.as_deref() {
-		None => return Err(Error::new(ErrorKind::InvalidInput, "Argument 1 must be filename or -")),
-		Some("-") => either::Left(BufReader::new(stdin())),
-		Some(x) => either::Right(BufReader::new(std::fs::File::open(x)?))
+	let mut args = std::env::args().fuse();
+
+	let mut elves = {
+		let filename = args.nth(1);
+		let input: Either<BufReader<Stdin>, BufReader<File>> = match filename.as_deref() {
+			None => return Err(Error::new(ErrorKind::InvalidInput, "Argument 1 must be filename or -")),
+			Some("-") => either::Left(BufReader::new(stdin())),
+			Some(x) => either::Right(BufReader::new(std::fs::File::open(x)?))
+		};
+		let lines = input.lines();
+		
+		let mut elves: Vec<IVec2> = Default::default();
+
+		for (y,line) in lines.enumerate() {
+			let line = line?;
+			let line = line.trim_end();
+			if line.is_empty() { break } // NOT DONE
+
+			for (x,ch) in line.chars().enumerate() {
+				if ch == '#' {
+					elves.push(IVec2::new(x as i32,y as i32));
+				}
+			}
+		}
+
+		elves
 	};
 
-	let lines = input.lines();
+	if elves.is_empty() { return Err(Error::new(ErrorKind::InvalidInput, "No elves?")) }
 
-	let mut total: i64 = 0;
+	for round in 0.. {
+		let mut elves_map:HashSet<IVec2> = Default::default();
+		let mut elves_min = IVec2::new(i32::MAX, i32::MAX);
+		let mut elves_max = IVec2::new(i32::MIN, i32::MIN);
 
-//	let invalid = || { return Err(Error::new(ErrorKind::InvalidInput, "Expecting other")) };
+		for &elf in elves.iter() {
+			elves_min = elves_min.min(elf);
+			elves_max = elves_max.max(elf);
+			elves_map.insert(elf);
+		}
 
-	// Scan file
-	for line in lines {
-		let line = line?;
+		if DEBUG_ROUND > 0 && round % DEBUG_ROUND == 0 {
+			let mut empty = 0;
+			for y in elves_min.y..=elves_max.y {
+				for x in elves_min.x..=elves_max.x {
+					print!("{}",
+						if elves.contains(&IVec2::new(x,y)) {
+							'#'
+						} else {
+							empty += 1;
+							'.'
+						}
+					)
+				}
+				println!("");
+			}
+			println!("Score: {}\n", empty);
+		}
+
+		break
 	}
-
-	// Final score
-	println!("{}", total);
 
 	Ok(())
 }
